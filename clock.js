5ed72f82e3b28fc3c486936102555460910f74fa
cyang001 <cyang001@citymail.cuny.edu>
2016-01-03 20:29:11 -0500

    Save and load object

--- clock.js
+++ clock.js
@@ -1,10 +1,20 @@
 // Pass by reference of an object
 var o = { totalSec: 10000*60*60 };
 
+
+function loadSeconds() {
+    o.totalSec = localStorage["seconds"];
+}
+
+loadSeconds();
+
 // Call every second
 var timeInterval = setInterval(function(){myTimer(o)}, 1000);
 
 var myTimer = function(o){
+    // Saves to local storage
+    localStorage["seconds"] = o.totalSec;
+    
     o.totalSec--;
     $("#hours").html(Math.floor(o.totalSec / 3600));
     $("#minutes").html(Math.floor(o.totalSec / 60) % 60);
