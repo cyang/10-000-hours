90fde5f3ca1ed6f1c3980c8daf8d0f242290acba
cyang001 <cyang001@citymail.cuny.edu>
2016-01-13 11:53:58 -0500

    Handle boundary cases

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function() {
     loadTasks();
 
     $('.collapsible').collapsible({
-      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+        accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
 
-    $('.btn-floating').leanModal();
+    $('#addButton').leanModal();
 
     $('select').material_select();
 });
 
-$(window).unload(function(){
+$(window).unload(function() {
     // Saves to chrome storage
-    chrome.storage.sync.set({'seconds': o.totalSec});
+    chrome.storage.sync.set({
+        'seconds': o.totalSec
+    });
     console.log("Saved");
 });
 
-$("#createTask").submit(function(e){
+$("#createTask").submit(function(e) {
     appendCollapsibleItem($("#color").val(), $("#title").val(), $("#description").val());
 
     $('#modal1').closeModal();
@@ -30,35 +32,49 @@ $("#createTask").submit(function(e){
     return false;
 });
 
-function updateTasks(){
+$("#removeButton").on("click", function(e) {
+    if (confirm("Are you sure you want to delete all notes/tasks?")){
+        chrome.storage.sync.remove('tasks');
+        $("#todo ul").html("");
+        loadTasks();
+    }
+});
+
+function updateTasks() {
     var task = {
         title: $("#title").val(),
         color: $("#color").val(),
         description: $("#description").val()
     };
 
-    chrome.storage.sync.get('tasks', function (obj) {
+
+    chrome.storage.sync.get('tasks', function(obj) {
+        if (obj['tasks'] === undefined) {
+            obj['tasks'] = [];
+        }
+
         obj['tasks'].push(task);
 
-        chrome.storage.sync.set(obj, function(){
+        chrome.storage.sync.set(obj, function() {
             console.log("Saved a new array item");
         });
     });
+
 }
 
-function loadTasks(){
-    chrome.storage.sync.get('tasks', function (obj) {
-        obj["tasks"].forEach(function(entry) {
-            appendCollapsibleItem(entry.color, entry.title, entry.description);
-            loadRemoveSelect();
-        });
+function loadTasks() {
+    chrome.storage.sync.get('tasks', function(obj) {
+
+        try {
+            obj["tasks"].forEach(function(entry) {
+                appendCollapsibleItem(entry.color, entry.title, entry.description);
+            });
+        } catch (err) {
+            console.log(err.message);
+        }
     });
 }
 
-function appendCollapsibleItem(color, title, description){
-    $("#todo ul").append( '<li><div class="collapsible-header"><i class="fa fa-circle-o" style="color: ' + color + '"></i>' + title + '</div><div class="collapsible-body"><p>' + description + '</p></div></li>' );
-}
-
-function loadRemoveSelect(){
-    $("#removeSelect").append('<option value="1">Option 1</option>');
+function appendCollapsibleItem(color, title, description) {
+    $("#todo ul").append('<li><div class="collapsible-header"><i class="fa fa-circle-o" style="color: ' + color + '"></i>' + title + '</div><div class="collapsible-body"><p>' + description + '</p></div></li>');
 }
